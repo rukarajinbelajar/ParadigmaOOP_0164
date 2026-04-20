57f73e30f20cec8906e8ed95e77e2c2a38de8c9f
rukarajinbelajar <maar1101dnd@gmail.com>
2026-04-20 14:41:46 +0700

    membuat program input

--- PrakOOP.cpp
+++ PrakOOP.cpp
@@ -3,12 +3,15 @@ using namespace std;
 
 class barang{
     public :
-    string nama;
+    string namaBarang;
     int jumlah;
     string kategori;
     string tanggalProduksi;
 
     void printData(){
-
+        cout << "Nama Barang : " <<namaBarang << endl;
+        cout << "Jumlah Barang : " << jumlah << endl;
+        cout << "Kategori Barang : " << kategori << endl;
+        cout << "Tanggal Produksi Barang : " << tanggalProduksi << endl;
     }
 }
