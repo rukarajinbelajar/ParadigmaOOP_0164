3ce8178bd14d06ec459a7c358a172be615bc819f
rukarajinbelajar <maar1101dnd@gmail.com>
2026-04-20 14:22:29 +0700

    membuat method dengan menampilkan input nim, nama, nilai

--- introductionOOP.cpp
+++ introductionOOP.cpp
@@ -6,4 +6,10 @@ class Mahasiswa{
     int nim;
     string nama;
     float nilai;
+
+    void printData(){
+        cout << "NIM : " << nim << endl;
+        cout << "Nama : " << nama << endl;
+        cout << "Nilai : " << nilai << endl;
+    }
 }
