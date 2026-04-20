8e3d9f200afa37f8f5e184b1505212d506eeef65
rukarajinbelajar <maar1101dnd@gmail.com>
2026-04-20 14:49:23 +0700

    membuat program utama dengan isi itu

--- PrakOOP.cpp
+++ PrakOOP.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-class barang{
+class Barang{
     public :
     string namaBarang;
     int jumlah;
@@ -15,3 +15,19 @@ class barang{
         cout << "Tanggal Produksi Barang : " << tanggalProduksi << endl;
     }
 }; //batas class
+
+int main (){
+    Barang Elektronik;
+    Elektronik.namaBarang = "Kulkas";
+    Elektronik.jumlah = 10;
+    Elektronik.kategori = "Elektronik";
+    Elektronik.tanggalProduksi = 20-25-2025;
+
+    Barang NonElektronik;
+    NonElektronik.namaBarang = "Meja";
+    NonElektronik.jumlah = 50;
+    NonElektronik.kategori = "Non Elektronik";
+    NonElektronik.tanggalProduksi = 11-12-2025;
+
+    
+}
